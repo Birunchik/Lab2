edc344b9d4432f0454ce956e51deea08a4539048
Birunchik <stassqw110@mail.com>
2026-09-17 00:15:56 +0300

    fuechers:        -finish PlayerClass        -make Template GameClass

---
+++ Lab2/Game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2 {
+     class Game {
+        public int size;
+        public Player Cat { get; }
+        public Player Mouse { get; }
+        public bool gameStatus;
+
+
+        String inputFile { get; }
+        String outputFile { get; }
+
+        public Game(int size, Player First, Player Second, String inp, String outp) {
+            Cat = First;
+            Mouse = Second;
+            gameStatus = true;
+            this.size = size;
+            inputFile = inp;
+            outputFile = outp;
+        }
+
+        public void Run() {
+            while (gameStatus) {
+                //парс из файла 
+                char com = 'O';
+                int step = 0;
+                MoveCommanD(com, step);
+                int dist = GetDist();
+                PrintInf();
+                if(dist == 0) gameStatus = false;
+            }
+        }
+
+        private void MoveCommanD(char command, int steps) {
+            switch (command) {
+                case 'C':
+                    Cat.Move(steps, size);
+                    break;
+                case 'M':
+                    Mouse.Move(steps, size);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void PrintInf() {
+              
+        }
+        private int GetDist() {
+            if (Mouse.state == State.NotInGame || Cat.state == State.NotInGame) {
+                return -1;
+            }
+            return Math.Abs(Cat.location - Mouse.location);
+            
+        }
+        
+    }
+}
---
+++ Lab2/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+
+namespace Lab2 {
+    class Player {
+        public String Name { get; private set; }
+        //поприкалываться
+        public int mmr {  get; private set; }
+        public int location { get; private set; }
+        public State state { get; private set; }
+        public int DistanceTraveled = 0;
+
+        public Player(String name) {
+            Name = name;
+            state = State.NotInGame;
+            location = -1;
+        }
+
+        // логика внутри или вне?? location = newPos 
+        // наверное внутри изза инкапсуляции
+        public void Move(int steps, int sizeGame) {
+            location = Math.Abs((steps + location) % sizeGame);
+        }
+    }
+
+    enum State {
+        Winner,
+        Looser,
+        Playing,
+        NotInGame
+    }
+}
