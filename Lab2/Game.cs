aee84c555de4844b49d653b10a85bc0f7d243db3
Birunchik <stassqw110@mail.com>
2026-09-17 13:26:32 +0300

    -work on GameClass

--- Lab2/Game.cs
+++ Lab2/Game.cs
@@ -13,28 +13,30 @@ namespace Lab2 {
         String inputFile { get; }
         String outputFile { get; }
 
-        public Game(int size, Player First, Player Second, String inp, String outp) {
+        public Game(Player First, Player Second, String inp, String outp) {
             Cat = First;
             Mouse = Second;
             gameStatus = true;
-            this.size = size;
             inputFile = inp;
             outputFile = outp;
         }
 
         public void Run() {
-            while (gameStatus) {
-                //парс из файла 
-                char com = 'O';
-                int step = 0;
-                MoveCommanD(com, step);
-                int dist = GetDist();
-                PrintInf();
-                if(dist == 0) gameStatus = false;
+            using (StreamReader sr = new StreamReader(inputFile)) {
+                String? line;
+                while (gameStatus && (line = sr.ReadLine()) != null) {
+                    String[] arr = line.Split(' ');
+                    char com = arr[0].Trim()[0];
+                    int steps;
+                    int.TryParse(arr[1], out steps);
+                        ReadCommanD(com, steps);
+
+                 
+                }
             }
         }
 
-        private void MoveCommanD(char command, int steps) {
+        private void ReadCommanD(char command, int steps) {
             switch (command) {
                 case 'C':
                     Cat.Move(steps, size);
@@ -42,12 +44,39 @@ namespace Lab2 {
                 case 'M':
                     Mouse.Move(steps, size);
                     break;
+                case 'P':
+                    PrintInf();
+                    break;
                 default:
                     break;
             }
         }
         private void PrintInf() {
-              
+            string catPos = Cat.state == State.NotInGame ? "??" : Cat.location.ToString();
+
+            string mousePos = Mouse.state == State.NotInGame ? "??" : Mouse.location.ToString();
+
+            int dist = GetDist();
+            string distStr = dist == -1 ? "" : dist.ToString();
+
+            PrintHeader();
+            Console.WriteLine($"{catPos,5}{mousePos,7}{distStr,10}");
+            
+            
+
+        }
+        public void PrintSummary() {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Distance traveled:   Mouse    Cat");
+            Console.WriteLine($"{Mouse.DistanceTraveled,22}{Cat.DistanceTraveled,7}");
+            Console.WriteLine();
+
+        }
+        public void PrintHeader() {
+            Console.WriteLine("Cat and Mouse\n");
+            Console.WriteLine("  Cat  Mouse   Distance");
+            Console.WriteLine("-----------------------");
         }
         private int GetDist() {
             if (Mouse.state == State.NotInGame || Cat.state == State.NotInGame) {
--- Lab2/Player.cs
+++ Lab2/Player.cs
@@ -6,22 +6,21 @@ using System.Text;
 namespace Lab2 {
     class Player {
         public String Name { get; private set; }
-        //поприкалываться
-        public int mmr {  get; private set; }
         public int location { get; private set; }
-        public State state { get; private set; }
+        public State state { get; set; }
         public int DistanceTraveled = 0;
 
         public Player(String name) {
             Name = name;
             state = State.NotInGame;
-            location = -1;
+            location = 0;
         }
 
-        // логика внутри или вне?? location = newPos 
-        // наверное внутри изза инкапсуляции
+        // логика внутри изза инкапсуляции
         public void Move(int steps, int sizeGame) {
+            state = State.Playing;
             location = Math.Abs((steps + location) % sizeGame);
+            DistanceTraveled += Math.Abs(steps);
         }
     }
 
